2a07d70ce4b77a30404f030942b53b802a05a498
guilhermedom <guilhermedfs@gmail.com>
2022-08-25 20:37:27 -0300

    style: reformat app script for better readability

--- app/app.R
+++ app/app.R
@@ -6,9 +6,12 @@ cars = read.csv("car.data", sep = ",")
 
 NBmodel = naiveBayes(class ~ ., cars)
 
-buying = c("Low" = "low", "Medium" = "med", "High" = "high", "Very high" = "vhigh")
-maintenance = c("Low" = "low", "Medium" = "med", "High" = "high", "Very high" = "vhigh")
-doors = c("Two" = "2", "Three" = "3", "Four" = "4", "More than four" = "5more")
+buying = c("Low" = "low", "Medium" = "med", "High" = "high",
+           "Very high" = "vhigh")
+maintenance = c("Low" = "low", "Medium" = "med", "High" = "high",
+                "Very high" = "vhigh")
+doors = c("Two" = "2", "Three" = "3", "Four" = "4",
+          "More than four" = "5more")
 persons = c("Two" = "2", "Four" = "4", "More than four" = "more")
 lug_boot = c("Small" = "small", "Medium" = "med", "Big" = "big")
 safety = c("Low" = "low", "Medium" = "med", "High" = "high")
@@ -20,23 +23,31 @@ ui = fluidPage(
     # Application title
     titlePanel(h1(align = "center", "Car Quality Estimation"),
                windowTitle = "Car Quality Estimation"),
-    helpText(align = "center", "Input informations about the car and click the \'Estimate!\' button to have a car quality estimation."),
+    helpText(align = "center", "Input informations about the car and click the
+             \'Estimate!\' button to have a car quality estimation."),
     
     br(),
     br(),
     
     fluidRow(
-        column(4, selectInput("buyingID", "Price:", choices = buying)),
-        column(4, selectInput("maintenanceID", "Maintenance Level:", choices = maintenance)),
-        column(4, selectInput("doorsID", "Number of doors:", choices = doors))
+        column(4, selectInput("buyingID", "Price:",
+                              choices = buying)),
+        column(4, selectInput("maintenanceID", "Maintenance Level:",
+                              choices = maintenance)),
+        column(4, selectInput("doorsID", "Number of doors:",
+                              choices = doors))
     ),
     fluidRow(
-        column(4, selectInput("personsID", "Seating capacity:", choices = persons)),
-        column(4, selectInput("lug_bootID", "Luggage size:", choices = lug_boot)),
-        column(4, selectInput("safetyID", "Safety:", choices = safety))
+        column(4, selectInput("personsID", "Seating capacity:",
+                              choices = persons)),
+        column(4, selectInput("lug_bootID", "Luggage size:",
+                              choices = lug_boot)),
+        column(4, selectInput("safetyID", "Safety:",
+                              choices = safety))
     ),
     fluidRow(
-        column(12, actionButton("estimateButtonID", "Estimate!"), h2(textOutput("resultID")))
+        column(12, actionButton("estimateButtonID", "Estimate!"),
+               h3(textOutput("resultID")))
     )
 
 )
@@ -44,17 +55,23 @@ ui = fluidPage(
 # Define server logic
 server = function(input, output) {
     observeEvent(input$estimateButtonID, {
-        carToPredict = data.frame("buying" = input$buyingID, "maintenance" = input$maintenanceID,
-                                  "doors" = input$doorsID, "persons" = input$personsID,
-                                  "lug_boot" = input$lug_bootID, "safety" = input$safetyID)
+        carToPredict = data.frame("buying" = input$buyingID,
+                                  "maintenance" = input$maintenanceID,
+                                  "doors" = input$doorsID,
+                                  "persons" = input$personsID,
+                                  "lug_boot" = input$lug_bootID,
+                                  "safety" = input$safetyID)
         
         prediction = as.character(predict(NBmodel, carToPredict))
-        prediction[prediction == "acc"] = "acceptable"
-        prediction[prediction == "unacc"] = "unacceptable"
-        prediction[prediction == "vgood"] = "very good"
+        prediction[prediction == "acc"] = "acceptable."
+        prediction[prediction == "unacc"] = "unacceptable."
+        prediction[prediction == "good"] = "good."
+        prediction[prediction == "vgood"] = "very good."
         
         prediction = paste0("Estimated car quality: ", prediction)
-        output$resultID = renderText({prediction})
+        output$resultID = renderText({
+            prediction
+        })
     })
 }
 
