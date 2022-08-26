19361edea8c0724e483b3bcceaec430f1d101359
guilhermedom <guilhermedfs@gmail.com>
2022-08-25 23:22:03 -0300

    docs: add comments to the model testing script

--- src/models/naive_bayes_testing.R
+++ src/models/naive_bayes_testing.R
@@ -5,15 +5,19 @@ library(e1071)
 
 cars = read.csv("../../data/raw/car.data", sep = ",")
 
+# Split dataset in train and test data with 70% being train data.
 carsSample = sample(2, 1728, replace = T, prob = c(0.7, 0.3))
 carsTrain = cars[carsSample == 1,]
 carsTest = cars[carsSample == 2,]
 
+# Naive Bayes model from the e1071 library.
 NBmodel = naiveBayes(class ~ ., carsTrain)
 prediction = predict(NBmodel, carsTest)
 
 confusionMatrix = table(carsTest$class, prediction)
 
-accuracy = ((confusionMatrix[1] + confusionMatrix[6] + confusionMatrix[11] + confusionMatrix[16]) / sum(confusionMatrix))
+# Measure accuracy using the prediction confusion matrix.
+accuracy = ((confusionMatrix[1] + confusionMatrix[6] +
+               confusionMatrix[11] + confusionMatrix[16]) / sum(confusionMatrix))
 
 show(accuracy)
